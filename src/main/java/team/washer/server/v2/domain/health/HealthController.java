36bc17ada5ea016806b03612fc5e6866ca812deb
snowykte0426 <snowykte0426@naver.com>
2025-10-23 13:25:50 +0900

    update: 헬스체크 엔드포인트 확장

--- src/main/java/team/washer/server/v2/domain/health/HealthController.java
+++ src/main/java/team/washer/server/v2/domain/health/HealthController.java
@@ -1,9 +1,9 @@
 package team.washer.server.v2.domain.health;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import team.washer.server.v2.global.common.response.data.response.CommonApiResDto;
 
 @RestController
@@ -12,5 +12,15 @@ public class HealthController {
     @GetMapping
     public CommonApiResDto healthCheck() {
         return CommonApiResDto.success("OK");
+    }
+
+    @GetMapping("/error")
+    public CommonApiResDto errorCheck() {
+        return CommonApiResDto.error("ERROR", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @GetMapping("/create")
+    public CommonApiResDto createCheck() {
+        return CommonApiResDto.created("CREATED");
     }
 }
