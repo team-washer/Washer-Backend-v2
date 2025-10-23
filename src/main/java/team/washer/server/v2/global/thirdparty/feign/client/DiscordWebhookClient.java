5daaf8c7e90effdc80f677bb21692b140b741b99
snowykte0426 <snowykte0426@naver.com>
2025-10-23 11:52:42 +0900

    update: Feign 클라이언트에서 명시적인 설정 클래스 적용

--- src/main/java/team/washer/server/v2/global/thirdparty/feign/client/DiscordWebhookClient.java
+++ src/main/java/team/washer/server/v2/global/thirdparty/feign/client/DiscordWebhookClient.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import team.washer.server.v2.global.thirdparty.discord.data.DiscordWebhookPayload;
+import team.washer.server.v2.global.thirdparty.feign.config.FeignConfig;
 
-@FeignClient(name = "discordWebhook", url = "${third-party.discord.webhook-url}")
+@FeignClient(name = "discordWebhook", url = "${third-party.discord.webhook-url}", configuration = FeignConfig.class)
 public interface DiscordWebhookClient {
     @PostMapping
     void sendMessage(@RequestBody DiscordWebhookPayload payload);
