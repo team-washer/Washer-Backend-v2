642ede4bfcf68e4e0d797c92969abc7fbc8e227d
snowykte0426 <snowykte0426@naver.com>
2025-11-05 09:52:45 +0900

    update: Spotless 포맷팅 적용

--- src/main/java/team/washer/server/v2/global/config/ObjectMapperConfig.java
+++ src/main/java/team/washer/server/v2/global/config/ObjectMapperConfig.java
@@ -1,13 +1,14 @@
 package team.washer.server.v2.global.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ObjectMapperConfig {
