8fbed9aa0a272ab2837f4ec7cedabe577ee0f9e6
Kim Tae Eun <snowykte0426@naver.com>
2026-01-16 01:54:50 +0900

    remove: 미사용 QueryAllUsersService 삭제

--- src/main/java/team/washer/server/v2/domain/user/service/QueryAllUsersService.java
+++
@@ -1,7 +0,0 @@
-package team.washer.server.v2.domain.user.service;
-
-import team.washer.server.v2.domain.user.dto.UserListResponseDto;
-
-public interface QueryAllUsersService {
-    UserListResponseDto getAllUsers();
-}
