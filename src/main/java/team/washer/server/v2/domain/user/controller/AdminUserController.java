6240efc3f2b80f15cfc68d7976f99f0b964a8311
Kim Tae Eun <snowykte0426@naver.com>
2026-01-16 02:42:03 +0900

    add: 사용자 ID 파라미터에 @NotNull 검증 추가

--- src/main/java/team/washer/server/v2/domain/user/controller/AdminUserController.java
+++ src/main/java/team/washer/server/v2/domain/user/controller/AdminUserController.java
@@ -1,5 +1,6 @@
 package team.washer.server.v2.domain.user.controller;
 
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import team.washer.server.v2.domain.user.dto.UserListResponseDto;
 import team.washer.server.v2.domain.user.dto.UserResponseDto;
@@ -18,6 +20,7 @@ import team.washer.server.v2.domain.user.service.SearchUserService;
 @RestController
 @RequestMapping("/api/v2/admin/users")
 @RequiredArgsConstructor
+@Validated
 @Tag(name = "Admin User Management", description = "사용자 관리 API (관리자용)")
 public class AdminUserController {
 
@@ -36,7 +39,7 @@ public class AdminUserController {
 
     @GetMapping("/{id}")
     @Operation(summary = "사용자 상세 조회", description = "ID로 특정 사용자를 조회합니다")
-    public UserResponseDto getUserById(@Parameter(description = "사용자 ID") @PathVariable Long id) {
+    public UserResponseDto getUserById(@Parameter(description = "사용자 ID") @PathVariable @NotNull Long id) {
         return queryUserByIdService.getUserById(id);
     }
 }
