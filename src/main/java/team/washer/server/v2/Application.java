53a26e6a1d807c2e7ed664282f1a5fbdd1ce9c79
snowykte0426 <snowykte0426@naver.com>
2026-01-20 17:27:46 +0900

    update: 메인 클래스에서 스케줄링 설정 제거

--- src/main/java/team/washer/server/v2/Application.java
+++ src/main/java/team/washer/server/v2/Application.java
@@ -2,10 +2,8 @@ package team.washer.server.v2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class Application {
 
     static void main(String[] args) {
