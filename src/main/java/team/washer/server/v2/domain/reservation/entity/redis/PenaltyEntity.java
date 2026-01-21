6ef1b2c67144101c21b8cf390eee612a2c7b446f
snowykte0426 <snowykte0426@naver.com>
2026-01-21 16:00:20 +0900

    add: Redis CrudRepository 및 Entity 추가 (패널티, 일요일 예약 상태)

---
+++ src/main/java/team/washer/server/v2/domain/reservation/entity/redis/PenaltyEntity.java
@@ -0,0 +1,29 @@
+package team.washer.server.v2.domain.reservation.entity.redis;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@RedisHash(value = "reservation:penalty:user", timeToLive = 600) // 10 minutes
+public class PenaltyEntity {
+
+    @Id
+    private Long userId;
+
+    private LocalDateTime expiryTime;
+
+    @TimeToLive
+    @Builder.Default
+    private Long ttl = 600L;
+}
