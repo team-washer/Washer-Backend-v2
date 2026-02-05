7112b3e8fa2d01afaf04969b8f1163e32dff443a
snowykte0426 <snowykte0426@naver.com>
2026-02-06 02:28:37 +0900

    update: PropertiesScanConfig를 전체 패키지 스캔으로 변경 및 중복 설정 제거

--- src/main/java/team/washer/server/v2/global/config/PropertiesConfig.java
+++
@@ -1,9 +0,0 @@
-package team.washer.server.v2.global.config;
-
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ConfigurationPropertiesScan("team.washer.server.v2")
-public class PropertiesConfig {
-}
--- src/main/java/team/washer/server/v2/global/config/PropertiesScanConfig.java
+++ src/main/java/team/washer/server/v2/global/config/PropertiesScanConfig.java
@@ -3,9 +3,7 @@ package team.washer.server.v2.global.config;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Configuration;
 
-import team.washer.server.v2.global.security.data.CorsEnvironment;
-
 @Configuration
-@ConfigurationPropertiesScan(basePackageClasses = CorsEnvironment.class)
+@ConfigurationPropertiesScan("team.washer.server.v2")
 public class PropertiesScanConfig {
 }
