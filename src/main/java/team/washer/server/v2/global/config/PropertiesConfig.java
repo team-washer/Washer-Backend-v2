1e24326a394a5440164e8ca17ae7c60c4fa02a93
snowykte0426 <snowykte0426@naver.com>
2026-02-06 02:16:30 +0900

    add: ConfigurationProperties 자동 스캔을 위한 PropertiesConfig 추가

---
+++ src/main/java/team/washer/server/v2/global/config/PropertiesConfig.java
@@ -0,0 +1,9 @@
+package team.washer.server.v2.global.config;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationPropertiesScan("team.washer.server.v2")
+public class PropertiesConfig {
+}
