b0ff3b86688bdbd2cb892241d51a88cffeda2381
snowykte0426 <snowykte0426@naver.com>
2026-02-05 20:42:54 +0900

    update: SchedulerConfig의 customTaskScheduler 반환 타입을 TaskScheduler로 변경

--- src/main/java/team/washer/server/v2/global/config/SchedulerConfig.java
+++ src/main/java/team/washer/server/v2/global/config/SchedulerConfig.java
@@ -2,7 +2,7 @@ package team.washer.server.v2.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
@@ -20,7 +20,7 @@ public class SchedulerConfig implements SchedulingConfigurer {
     }
 
     @Bean(name = "customTaskScheduler")
-    public TaskExecutor customTaskScheduler() {
+    public TaskScheduler customTaskScheduler() {
         final var scheduler = new ThreadPoolTaskScheduler();
         scheduler.setPoolSize(5);
         scheduler.setThreadNamePrefix("Scheduler-");
