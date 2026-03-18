ab02882a7dddf44fbce47e19aa078fe27611a031
wwwcomcomcomcom <iieiiergn@gmail.com>
2026-03-18 18:21:42 +0900

    update: 세탁 완료 알림 서비스에 FCM 푸시 알림 전송 연동

--- src/main/java/team/washer/server/v2/domain/notification/service/impl/SendCompletionNotificationServiceImpl.java
+++ src/main/java/team/washer/server/v2/domain/notification/service/impl/SendCompletionNotificationServiceImpl.java
@@ -1,14 +1,15 @@
 package team.washer.server.v2.domain.notification.service.impl;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import team.washer.server.v2.domain.machine.entity.Machine;
 import team.washer.server.v2.domain.notification.entity.Notification;
+import team.washer.server.v2.domain.notification.enums.NotificationType;
 import team.washer.server.v2.domain.notification.repository.NotificationRepository;
 import team.washer.server.v2.domain.notification.service.SendCompletionNotificationService;
+import team.washer.server.v2.domain.notification.service.SendFcmNotificationService;
 import team.washer.server.v2.domain.user.entity.User;
 
 @Service
@@ -17,9 +18,9 @@ import team.washer.server.v2.domain.user.entity.User;
 public class SendCompletionNotificationServiceImpl implements SendCompletionNotificationService {
 
     private final NotificationRepository notificationRepository;
+    private final SendFcmNotificationService sendFcmNotificationService;
 
     @Override
-    @Transactional
     public void execute(User user, Machine machine) {
         try {
             var notification = Notification.createCompletionNotification(user, machine);
@@ -30,6 +31,14 @@ public class SendCompletionNotificationServiceImpl implements SendCompletionNoti
                     user.getId(),
                     machine.getName(),
                     e);
+        }
+
+        try {
+            final var fcmTitle = "세탁 완료 알림";
+            final var fcmBody = NotificationType.COMPLETION.formatMessage(machine.getName());
+            sendFcmNotificationService.execute(user, fcmTitle, fcmBody);
+        } catch (Exception e) {
+            log.error("Error sending FCM notification: userId={}, machineName={}", user.getId(), machine.getName(), e);
         }
     }
 }
