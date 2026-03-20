1ac692c81dfb811ebf8abb4bbcb2193e73b28b07
snowykte0426 <snowykte0426@naver.com>
2026-03-20 10:22:37 +0900

    update: FCM 설정 경로를 `third-party.fcm`으로 변경 및 구조 통일

--- src/main/java/team/washer/server/v2/global/config/FcmProperties.java
+++ src/main/java/team/washer/server/v2/global/config/FcmProperties.java
@@ -2,6 +2,6 @@ package team.washer.server.v2.global.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties("fcm")
+@ConfigurationProperties("third-party.fcm")
 public record FcmProperties(String serviceAccountJson) {
 }
