36f5316556ba217fd45a361aaee04ad978ea74bd
snowykte0426 <snowykte0426@naver.com>
2026-03-21 18:07:09 +0900

    update: .env.stage 방식에서 application-stage.yml 빌드 번들링 방식으로 CD 파이프라인 변경

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,7 +4,6 @@ cd /app/washer
 docker build -f stage.dockerfile -t washer-backend-v2:latest .
 docker run -d \
   --name washer-backend-v2 \
-  --env-file /app/washer/.env.stage \
   --network host \
   --restart unless-stopped \
   washer-backend-v2:latest
