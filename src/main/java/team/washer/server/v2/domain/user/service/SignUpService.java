9585c433f6c47442edaebc446013e42c39b06583
snowykte0426 <snowykte0426@naver.com>
2026-03-25 23:58:47 +0900

    update: User 도메인을 Support 레이어로 리팩터링

--- src/main/java/team/washer/server/v2/domain/user/service/SignUpService.java
+++
@@ -1,8 +0,0 @@
-package team.washer.server.v2.domain.user.service;
-
-import team.themoment.datagsm.sdk.oauth.model.Student;
-import team.washer.server.v2.domain.user.entity.User;
-
-public interface SignUpService {
-    User execute(Student student);
-}
--- src/main/java/team/washer/server/v2/domain/user/support/UserRegistrationSupport.java
+++ src/main/java/team/washer/server/v2/domain/user/support/UserRegistrationSupport.java
@@ -1,26 +1,27 @@
-package team.washer.server.v2.domain.user.service.impl;
+package team.washer.server.v2.domain.user.support;
 
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import team.themoment.datagsm.sdk.oauth.model.Student;
 import team.washer.server.v2.domain.user.entity.User;
 import team.washer.server.v2.domain.user.repository.UserRepository;
-import team.washer.server.v2.domain.user.service.SignUpService;
 
-@Service
+/**
+ * 신규 사용자 등록을 담당하는 지원 컴포넌트.
+ */
+@Component
 @RequiredArgsConstructor
-public class SignUpServiceImpl implements SignUpService {
+public class UserRegistrationSupport {
 
     private final UserRepository userRepository;
 
-    @Override
-    @Transactional
-    /*
-     * 메서드를 호출하기전 중복된 User가 없음을 보장해야합니다.
+    /**
+     * 신규 사용자를 등록한다. 메서드를 호출하기 전 중복된 User가 없음을 보장해야 합니다.
      */
-    public User execute(Student student) {
+    @Transactional
+    public User register(Student student) {
         User user = User.builder().studentId(student.getStudentNumber().toString()).name(student.getName())
                 .roomNumber(student.getDormitoryRoom().toString()).grade(student.getGrade())
                 .floor(student.getDormitoryFloor()).build();
