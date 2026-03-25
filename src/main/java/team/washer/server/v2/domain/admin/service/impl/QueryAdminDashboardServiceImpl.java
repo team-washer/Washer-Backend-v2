75c0436953597a8e0f837ae7aee6793434ed00f4
snowykte0426 <snowykte0426@naver.com>
2026-03-25 21:03:56 +0900

    update: 관리자 대시보드 통계 항목 추가

--- src/main/java/team/washer/server/v2/domain/admin/service/impl/QueryAdminDashboardServiceImpl.java
+++ src/main/java/team/washer/server/v2/domain/admin/service/impl/QueryAdminDashboardServiceImpl.java
@@ -1,5 +1,7 @@
 package team.washer.server.v2.domain.admin.service.impl;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -7,9 +9,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import team.washer.server.v2.domain.admin.dto.response.AdminDashboardResDto;
 import team.washer.server.v2.domain.admin.service.QueryAdminDashboardService;
+import team.washer.server.v2.domain.machine.enums.MachineStatus;
+import team.washer.server.v2.domain.machine.repository.MachineRepository;
 import team.washer.server.v2.domain.malfunction.enums.MalfunctionReportStatus;
 import team.washer.server.v2.domain.malfunction.repository.MalfunctionReportRepository;
 import team.washer.server.v2.domain.reservation.repository.ReservationRepository;
+import team.washer.server.v2.domain.user.repository.UserRepository;
+import team.washer.server.v2.global.common.constants.PenaltyConstants;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +24,8 @@ public class QueryAdminDashboardServiceImpl implements QueryAdminDashboardServic
 
     private final ReservationRepository reservationRepository;
     private final MalfunctionReportRepository malfunctionReportRepository;
+    private final MachineRepository machineRepository;
+    private final UserRepository userRepository;
 
     @Override
     @Transactional(readOnly = true)
@@ -28,8 +36,18 @@ public class QueryAdminDashboardServiceImpl implements QueryAdminDashboardServic
         var pendingReports = malfunctionReportRepository.countByStatus(MalfunctionReportStatus.PENDING);
         var processingReports = malfunctionReportRepository.countByStatus(MalfunctionReportStatus.IN_PROGRESS);
         var completedReports = malfunctionReportRepository.countByStatus(MalfunctionReportStatus.RESOLVED);
+        var totalMachines = machineRepository.count();
+        var malfunctionMachines = machineRepository.countByStatus(MachineStatus.MALFUNCTION);
+        var penaltyThreshold = LocalDateTime.now().minusMinutes(PenaltyConstants.PENALTY_DURATION_MINUTES);
+        var suspendedStudents = userRepository.countSuspendedStudents(penaltyThreshold);
 
-        var result = new AdminDashboardResDto(activeReservations, pendingReports, processingReports, completedReports);
+        var result = new AdminDashboardResDto(activeReservations,
+                pendingReports,
+                processingReports,
+                completedReports,
+                totalMachines,
+                malfunctionMachines,
+                suspendedStudents);
 
         log.info("Successfully queried admin dashboard statistics");
 
