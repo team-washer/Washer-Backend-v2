1a5cc9d439a16734b3080cca27645afd7485537c
snowykte0426 <snowykte0426@naver.com>
2026-04-07 14:03:34 +0900

    update: 로그인 요청 시 리다이렉트 URI를 파라미터로 받도록 변경

--- src/main/java/team/washer/server/v2/domain/auth/service/impl/SignInServiceImpl.java
+++ src/main/java/team/washer/server/v2/domain/auth/service/impl/SignInServiceImpl.java
@@ -16,13 +16,11 @@ import team.washer.server.v2.domain.auth.util.WithdrawnStudentRedisUtil;
 import team.washer.server.v2.domain.user.entity.User;
 import team.washer.server.v2.domain.user.repository.UserRepository;
 import team.washer.server.v2.domain.user.support.UserRegistrationSupport;
-import team.washer.server.v2.global.thirdparty.datagsm.config.DataGsmEnvironment;
 
 @Service
 @AllArgsConstructor
 public class SignInServiceImpl implements SignInService {
     private final DataGsmOAuthClient oauthClient;
-    private final DataGsmEnvironment dataGsmEnvironment;
     private final UserRepository userRepository;
     private final UserRegistrationSupport userRegistrationSupport;
     private final TokenGenerationSupport tokenGenerationSupport;
@@ -30,7 +28,7 @@ public class SignInServiceImpl implements SignInService {
 
     @Override
     public TokenResDto execute(TokenReqDto reqDto) {
-        String accessToken = oauthClient.exchangeCodeForToken(reqDto.authCode(), dataGsmEnvironment.redirectUri())
+        String accessToken = oauthClient.exchangeCodeForToken(reqDto.authCode(), reqDto.redirectUri())
                 .getAccessToken();
         Student oauthUser = oauthClient.getUserInfo(accessToken).getStudent();
         if (oauthUser == null) {
