828b159d1a1b172cda2750e8d2538907c59cc62d
snowykte0426 <snowykte0426@naver.com>
2026-04-27 00:19:41 +0900

    add: Washer CI 워크플로우 추가 및 배포 스크립트 보완

--- scripts/prod/start.sh
+++ scripts/prod/start.sh
@@ -4,7 +4,7 @@ cd /home/ec2-user/builds
 docker build -f prod.dockerfile -t washer-backend-v2:latest .
 docker run -d \
   --name washer-backend-v2 \
-  --network host \
+  -p 8080:8080 \
   --restart unless-stopped \
   washer-backend-v2:latest
 docker image prune -f
