5552a5a1f64d4f4a1b80288de703b7c015e9b8b7
snowykte0426 <snowykte0426@naver.com>
2026-05-22 18:39:46 +0900

    update: 5층 사용자 기기 목록 조회 제한 추가 (451 반환)

--- src/main/java/team/washer/server/v2/domain/machine/service/QueryAllMachinesStatusService.java
+++ src/main/java/team/washer/server/v2/domain/machine/service/QueryAllMachinesStatusService.java
@@ -5,5 +5,5 @@ import java.util.List;
 import team.washer.server.v2.domain.machine.dto.response.MachineStatusResDto;
 
 public interface QueryAllMachinesStatusService {
-    List<MachineStatusResDto> execute(boolean sorted);
+    List<MachineStatusResDto> execute(Long userId, boolean sorted);
 }
