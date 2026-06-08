96b6b93305a21aea91532aac3bcd4dfecee83835
snowykte0426 <snowykte0426@naver.com>
2026-06-08 11:01:10 +0900

    fix(user): DataGSM Feign 클라이언트 Encoder 빈 누락으로 인한 기동 실패 수정

--- src/main/java/team/washer/server/v2/global/thirdparty/datagsm/feign/DataGsmOpenApiClient.java
+++ src/main/java/team/washer/server/v2/global/thirdparty/datagsm/feign/DataGsmOpenApiClient.java
@@ -7,8 +7,9 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import team.washer.server.v2.global.thirdparty.datagsm.dto.response.DataGsmStudentSearchResDto;
+import team.washer.server.v2.global.thirdparty.feign.config.FeignConfig;
 
-@FeignClient(name = "datagsm-openapi", url = "${third-party.datagsm.openapi-url:https://openapi.datagsm.kr}")
+@FeignClient(name = "datagsm-openapi", url = "${third-party.datagsm.openapi-url:https://openapi.datagsm.kr}", configuration = FeignConfig.class)
 public interface DataGsmOpenApiClient {
 
     @GetMapping(value = "/v1/students", produces = MediaType.APPLICATION_JSON_VALUE)
