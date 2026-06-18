a4647c3cdd38f71b85d573f4cbc343759e9d8431
d1ng1724 <s25004@gsm.hs.kr>
2026-06-18 19:58:29 +0900

    delete(appversion): Admin App Version 기능 제거

--- src/main/java/team/washer/server/v2/domain/appversion/service/UpsertAppVersionPolicyService.java
+++
@@ -1,9 +0,0 @@
-package team.washer.server.v2.domain.appversion.service;
-
-import team.washer.server.v2.domain.appversion.dto.request.UpsertAppVersionPolicyReqDto;
-import team.washer.server.v2.domain.appversion.dto.response.AppVersionPolicyResDto;
-import team.washer.server.v2.domain.appversion.enums.AppPlatform;
-
-public interface UpsertAppVersionPolicyService {
-    AppVersionPolicyResDto execute(AppPlatform platform, UpsertAppVersionPolicyReqDto reqDto);
-}
